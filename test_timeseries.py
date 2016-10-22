bc017531fc68b4341f9c47de73cb28d8ea972c27
lab5_Yihang <yihang_yan@g.harvard.edu>
2016-10-22 16:42:07 -0400

    update file

--- test_timeseries.py
+++ test_timeseries.py
@@ -1,15 +1,35 @@
-import unittest
-from pytest import raises
+import pytest
+import types
 import numpy as np
 from timeseries import TimeSeries
 
-class MyTest(unittest.TestCase):
+def test_init():
+	t = [1, 3, 4.5, 6, 9]
+	v = [0, 4, 1, 1.5, 2]
+	z = TimeSeries(t,v)
+	assert z[4.5] == 1.5
+	assert z[1] != 1
+	z[1.5] = 2.5
+	assert a[1.5] == 2.5
+	print("hi")
 
-	def test_getitem(self):
-		key = 3
-		series = TimeSeries(range(1000))
-		self.assertEqual(series[key], 3)
+def test_len():
+	t = [1, 3, 4.5, 6, 9]
+	v = [0, 4, 1, 1.5, 2]
+	a = TimeSeries(t, v)
+    assert len(a) == 5
+    a = TimeSeries([], [])
+    assert len(a) == 0
+
+def test_iters():
+
+    t = [1, 1.5, 2, 2.5, 10]
+    v = [0, 2, -1, 0.5, 0]
+    a = TimeSeries(t, v)
+
+    assert list(a.times()) == t
+    assert list(a.values()) == v
 
 
-suite = unittest.TestLoader().loadTestsFromModule(MyTest())
-unittest.TextTestRunner().run(suite)
+
+
--- timeseries.py
+++ timeseries.py
@@ -3,32 +3,199 @@ import reprlib
 
 class TimeSeries:
     
-    def __init__(self, data):
-        for datum in data:
-            try:
-                int(datum)
-            except ValueError:
-                print('[Error] List has values that are not numbers.')
-                return
-        self._timeseries = list(data)
+    def __init__(self, times, values):
+        
+        '''
+
+        >>> t = [1.5, 2, 2.5, 3, 10.5]
+        >>> v = [1, 3, 0, 1.5, 1]
+        >>> z = TimeSeries(t,v)
+        >>> z[3]
+        1.5
+
+        '''
+
+        #numpy - provide a length of the sequence
+        times = np.array(times, dtype=float)
+        values = np.array(values, dtype=float)
+
+        #sort ties in ascending order
+        sort_order = np.argsort(times)
+        times = times[sort_order]
+        values = values[sort_order]
+
+        #create private propterty
+        self.__timesseq = np.array(times)
+        self.__valuesseq = np.array(values)
+        self.__times_to_index = {t: i for i, t in enumerate(times)}
+
+    @ propterty
+    def timesseq(self):
+        '''
+        Time serires index
+        Priviate property - can't be called directly
+        '''
+        return self.__timesseq
+
+
+    @ propterty
+    def valuesseq(self):
+        '''
+        Time serires value
+        Priviate property - can't be called directly
+        '''
+        return self.__valuesseq
+
+    @property
+    def times_to_index(self):
+        '''
+        map time index with integer index of the array
+        Priviate property - can't be called directly
+        '''
+        return self.__times_to_index
+
+
+
+    def times(self):
+        '''
+        Returns the times sequence.
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        Numpy array
+            Time series times
+        >>> t = [1, 1.5, 2, 2.5, 10]
+        >>> v = [0, 2, -1, 0.5, 0]
+        >>> a = TimeSeries(t, v)
+        >>> a.times()
+        array([  1. ,   1.5,   2. ,   2.5,  10. ])
+        '''
+        return self.timesseq
+
+    def values(self):
+        '''
+        Returns the values sequence.
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        Numpy array
+            Time series values
+        >>> t = [1, 1.5, 2, 2.5, 10]
+        >>> v = [0, 2, -1, 0.5, 0]
+        >>> a = TimeSeries(t, v)
+        >>> a.values()
+        array([ 0. ,  2. , -1. ,  0.5,  0. ])
+        '''
+        return self.valuesseq
+
+    def items(self):
+        '''
+        Returns sequence of (time, value) tuples.
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        Numpy array of tuples
+            Time series time-value pairs
+        >>> t = [1, 1.5, 2, 2.5, 10]
+        >>> v = [0, 2, -1, 0.5, 0]
+        >>> a = TimeSeries(t, v)
+        >>> a.items()
+        [(1.0, 0.0), (1.5, 2.0), (2.0, -1.0), (2.5, 0.5), (10.0, 0.0)]
+        '''
+        return [(time, self[time]) for time in self.__timesseq]
+
+
 
     def __len__(self):
-        return len(self._timeseries)
 
-    def __getitem__(self, key):
+        '''
+        >>> t = [1.5, 2, 2.5, 3, 10.5]
+        >>> v = [1, 3, 0, 1.5, 1]
+        >>> a = TimeSeries(t, v)
+        >>> len(a)
+        5
+        >>> len(TimeSeries([], []))
+        0
+        '''
+        return len(self.timesseq)
+
+
+    def __getitem__(self, time):
+
+        '''
+         >>> t = [1.5, 2, 2.5, 3, 10.5]
+        >>> v = [1, 3, 0, 1.5, 1]
+        >>> a = TimeSeries(t, v)
+        >>> a[2.5]
+        0
+        '''
+
         if key >= self.__len__():
             raise IndexError('Index chosen is out of range.')
-        return self._timeseries[key]
+        return self.__valuesseq[self.times_to_index[float(time)]]
 
-    def __setitem__(self, key, value):
+
+
+
+    def __setitem__(self, time, value):
+
+        '''
+
+        >>> t = [1.5, 2, 2.5, 3, 10.5]
+        >>> v = [1, 3, 0, 1.5, 1]
+        >>> a = TimeSeries(t, v)
+        >>> a[1] = 12.0
+        >>> a[1]
+        12.0
+        >>> a[5] = 9.0
+        >>> a[5]
+        9.0
+        '''
         if key >= self.__len__():
             raise IndexError('Index chosen is out of range.')
         try:
-            int(value)
+            #int(value)
+            self.__valuesseq[self.times_to_index[float(times)]] = float(value)
         except ValueError:
             print('[Error] Value is not a number.')
             return
-        self._timeseries[key] = value
+        #self._timeseries[time] = value
+
+
+
+    def __str__(self):
+       '''
+       Printable representation of sequence
+
+        >>> t = [1.5, 2, 2.5, 3, 10.5]
+        >>> v = [1, 3, 0, 1.5, 1]
+        >>> a = TimeSeries(t, v)  # less than 6
+        >>> str(a)
+        'Time Series Length: 5 [1.0, 3.0, 0.0, 1.5, 1.0]'
+        >>> t = [1.5, 2, 2.5, 3, 10.5, 13, 16]
+        >>> v = [1, 3, 0, 1.5, 1, 2, 9]
+        >>> a = TimeSeries(t, v)  # longer than 6
+        >>> str(a)
+        'Length: 7 [1.0, 3.0, ..., 9.0]'
+       '''
+       
+        n = len(self)
+        if n > 6:
+            return('Time Series Length: {} [{},{},...,{}]'.format(
+            n, self[self.__timesseq[0]], self[self.__timesseq[1]], self[self.__timesseq[-1]]))
+        else:
+            list_time = ', '.join([v for v in self])
+            return('Time Series Length: {} [{}]'.format(n, list_time))
+
+
+
+
 
     def __repr__(self):
         class_name = type(self).__name__
@@ -37,13 +204,122 @@ class TimeSeries:
         components = myrepr.repr(self._timeseries)
         components = components[components.find('['):]
         return '{}({})'.format(class_name, components)   
+
+
+
+    def __contains__(self, time):
+        '''
+        Takes a time and returns true if it is in the times array.
+        Parameters
+        ----------
+        time : int or float
+            A time series time
+        Returns
+        -------
+        bool
+            Whether the time is present in the time series
+        >>> t = [1, 1.5, 2, 2.5, 10]
+        >>> v = [0, 2, -1, 0.5, 0]
+        >>> a = TimeSeries(t, v)
+        >>> 1 in a
+        True
+        >>> 3 in a
+        False
+        '''
+        return float(time) in self.times_to_index.keys()
     
-    def __str__(self):
-        # Shows the length and the first and last elements
-        class_name = type(self).__name__
-        first = 'N/A'
-        last = 'N/A'
-        if len(self._timeseries) > 0:
-            first = str(self._timeseries[0])
-            last = str(self._timeseries[-1])
-        return '%s\nLength: %d\nFirst: %s, Last: %s' % (class_name, len(self._timeseries), first, last)
+
+
+    def __iter__(self):
+        '''
+        >>> t = [1, 1.5, 2, 2.5, 10]
+        >>> v = [0, 2, -1, 0.5, 0]
+        >>> a = TimeSeries(t, v)
+        >>> for val in a:
+        ...     print(val)
+        0.0
+        2.0
+        -1.0
+        0.5
+        0.0
+        '''
+        for v in self.__valuesseq:
+            return v
+
+
+     def itertimes(self):
+        '''
+        Iterates over the times array.
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        float(s)
+            Iterator of time series times
+        >>> t = [1, 1.5, 2, 2.5, 10]
+        >>> v = [0, 2, -1, 0.5, 0]
+        >>> a = TimeSeries(t, v)
+        >>> for val in a.itertimes():
+        ...     print(val)
+        1.0
+        1.5
+        2.0
+        2.5
+        10.0
+        '''
+        for t in self.__timesseq:
+            yield t
+
+    def itervalues(self):
+        '''
+        Iterates over the values array.
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        float(s)
+            Iterator of time series values
+        >>> t = [1, 1.5, 2, 2.5, 10]
+        >>> v = [0, 2, -1, 0.5, 0]
+        >>> a = TimeSeries(t, v)
+        >>> for val in a.itervalues():
+        ...     print(val)
+        0.0
+        2.0
+        -1.0
+        0.5
+        0.0
+        '''
+        for v in self.__valuesseq:
+            yield v
+
+    def iteritems(self):
+        '''
+        Iterates over the time-values pairs.
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        tuple(s) of floats
+            Iterator of time series time-value pairs
+        >>> t = [1, 1.5, 2, 2.5, 10]
+        >>> v = [0, 2, -1, 0.5, 0]
+        >>> a = TimeSeries(t, v)
+        >>> for val in a.iteritems():
+        ...     print(val)
+        (1.0, 0.0)
+        (1.5, 2.0)
+        (2.0, -1.0)
+        (2.5, 0.5)
+        (10.0, 0.0)
+        '''
+        for t, v in zip(self.__timesseq, self.__valuesseq):
+            yield t, v
+
+
+
+
+
