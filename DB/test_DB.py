dea84130d1fc0467efdd0213697dac69e46da451
Tomas Gudmundsson <lavenderviking@Tomass-MacBook-Pro.local>
2016-11-30 15:00:47 -0500

    test_SMTimeSeries added

--- DB/test_DB.py
+++ DB/test_DB.py
@@ -2,6 +2,8 @@ import unittest
 import os
 from DB import *
 import sys
+import pickle
+import base64
 sys.path.append('../')
 from TimeSeries import TimeSeries
 
@@ -10,44 +12,230 @@ from TimeSeries import TimeSeries
 # Test cases for the DB class
 
 class DBTest(unittest.TestCase):
+	def getName(self):
+		return "Bainn1.dbdb"
 
-	# Test caching
-	# Test encode and decode directly
-
+	def height(self, node):
+		if node is None:
+			return 0
+		leftNode = node.left_ref._referent
+		rightNode = node.right_ref._referent
+		return max(self.height(leftNode) + 1, self.height(rightNode) + 1)
+	
+	def heightMin(self, node):
+		if node is None:
+			return 0
+		leftNode = node.left_ref._referent
+		rightNode = node.right_ref._referent
+		return min(self.heightMin(leftNode) + 1, self.heightMin(rightNode) + 1)
+	
+	def is_validly_balanced(self, node):
+		# The height of the longest path to leaf cannot be more than 2x larger than the height of the shortest path to leaf
+		return self.height(node) <= 2*self.heightMin(node)
+	
+	
 	def setUp(self):
-		dbname = 'binarytreetest.dbdb'
+		self.dbname = self.getName()
 		try:
-			f = open(dbname, 'r+b')
+			f = open(self.dbname, 'r+b')
 		except IOError:
-			fd = os.open(dbname, os.O_RDWR | os.O_CREAT)
+			fd = os.open(self.dbname, os.O_RDWR | os.O_CREAT)
+
 			f = os.fdopen(fd, 'r+b')
 
 		self.storage = Storage(f)
+		self.storage.lock()
+		self.rbtree = BinaryTree(self.storage)
+		self.rbtree.set(2, "99")
+		self.rbtree.set(4, "101")
+		self.rbtree.set(1, "98")
+		self.rbtree.set(3, "100")
+		self.rbtree.set(5, "102")
+		self.rbtree.set(6, "103")
+		self.rbtree.set(7, "104")
+		self.rbtree.set(8, "105")
+		self.	rbtree.set(2, "99")
+		self.	rbtree.set(4444, "101")
+		
 
+		"""	
+		self.	rbtree.set(4, "101")
+		self.	rbtree.set(4, "101")
+		self.	rbtree.set(4, "101")
+		self.	rbtree.set(4, "101")
+		self.	rbtree.set(4, "101")
+		self.	rbtree.set(1, "98")
+		self.	rbtree.set(3, "100")
+		self.	rbtree.set(5, "102")
+		self.	rbtree.set(6, "103")
+		self.	rbtree.set(7, "104")	
+		self.	rbtree.set(8, "105")
+		self.	rbtree.set(10, "105")
+		self.	rbtree.set(20, "105")
+		#self.	rbtree.set(30, "105")
+		#self.	rbtree.set(15, "105")
+		
+		
+		self.	rbtree.set(18, "105")
+		self.	rbtree.set(50, "105")
+		self.	rbtree.set(32, "105")
+		self.	rbtree.set(81, "105")
+		"""
+
+		self.root = self.rbtree._follow(self.rbtree._tree_ref)
 		self.ts = TimeSeries(values=[1, 3, 0, 1.5, 1], times=[1.5, 2, 2.5, 3, 10.5])
 		self.ts_notime = TimeSeries(values=[2, 3, 4], times=None)
 		self.ts_single = TimeSeries(values=[-2], times=[1])
 
 	def tearDown(self):
+		os.remove(self.getName())
+		del self.dbname
 		del self.storage
+		del self.rbtree
+		del self.root
 		del self.ts
 		del self.ts_notime
 		del self.ts_single
 
-	# Test store, get size, get time series
-	
-	def test_storeAndGetWithKey(self):
-		btree = BinaryTree(self.storage)
-		btree.set(2, "99")
-		btree.set(4, "101")
-		btree.set(1, "98")
-		btree.set(3, "100")
-		btree.set(5, "102")
-		btree.set(6, "103")
-		btree.set(7, "104")
-		btree.set(8, "105")
-		print(type(btree._tree_ref))
-		self.assertTrue(isinstance(btree, BinaryTree))
+	def test_isInstance_BinaryTree(self):
+		self.assertTrue(isinstance(self.rbtree, BinaryTree))
+
+	def test_rootIsBlack(self):
+		self.assertTrue(self.root.color == 1)
+
+	def test_BinaryTree_Root(self):
+		self.assertTrue(self.root.key == 4)
+
+	def test_BinaryTree_Left(self):
+		left = self.rbtree._follow(self.root.left_ref)
+		self.assertTrue(left.key == 2)
+
+	def test_BinaryTree_Right(self):
+		right = self.rbtree._follow(self.root.right_ref)
+		print (right.value_ref.address)
+		self.assertTrue(right.key == 6)
+
+	def test_root_address(self):
+		self.assertTrue(self.rbtree._tree_ref.address == 0)
+
+	def test_commit(self):
+		self.assertTrue(self.rbtree.commit() == None)
+
+	def test_get_value_binarytree(self):
+		value = self.rbtree.get(1)
+		print(value)
+		self.assertTrue(value == "98")
+
+	def test_lock(self):
+		self.storage.unlock()
+		value = self.rbtree.get(2)
+		print(value)
+		self.assertTrue(value == "98")
+
+	# TODO: Fix
+	def test_valueRef(self):
+		self.root.value_ref._referent = None
+		self.root.value_ref._address = 0xff
+		valueref = self.root.value_ref.get(self.storage)
+		self.assertTrue(valueref == '')
+
+	def test_Big_RBT_Tree(self):
+		rbtree = BinaryTree(self.storage)
+		rbtree.set(2, "99")
+		rbtree.set(4, "101")
+		rbtree.set(1, "98")
+		rbtree.set(3, "100")
+		rbtree.set(5, "102")
+		rbtree.set(6, "103")
+		rbtree.set(7, "104")
+		rbtree.set(8, "105")
+		rbtree.set(2, "99")
+		rbtree.set(4444, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(1, "98")
+		rbtree.set(3, "100")
+		rbtree.set(5, "102")
+		rbtree.set(6, "103")
+		rbtree.set(7, "104")	
+		rbtree.set(8, "105")
+		rbtree.set(10, "105")
+		rbtree.set(20, "105")
+
+		root = rbtree._follow(rbtree._tree_ref)
+		self.assertEquals(self.is_validly_balanced(root), 1)
+		#self.assertTrue(root.key == 4)
+
+
+	def test_Big_RBT_Tree2(self):
+		rbtree = BinaryTree(self.storage)
+		rbtree.set(2234, "99")
+		rbtree.set(2344, "101")
+		rbtree.set(1, "98")
+		rbtree.set(3, "100")
+		rbtree.set(5, "102")
+		rbtree.set(236, "103")
+		rbtree.set(7, "104")
+		rbtree.set(2228, "105")
+		rbtree.set(2, "99")
+		rbtree.set(4444, "101")
+		rbtree.set(422, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(774, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "234101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(14, "101")
+		rbtree.set(14, "101")
+		rbtree.set(444, "101")
+		rbtree.set(524, "101")
+		rbtree.set(4, "101")
+		rbtree.set(4, "101")
+		rbtree.set(3456541, "98")
+		rbtree.set(3, "100")
+		rbtree.set(5, "102")
+		rbtree.set(6, "103")
+		rbtree.set(57, "104")	
+		rbtree.set(8, "105")
+		rbtree.set(22310, "105")
+		rbtree.set(240, "105")
+		root = rbtree._follow(rbtree._tree_ref)
+		self.assertEquals(self.is_validly_balanced(root), 1)
+		#self.assertTrue(root.key == 7)
+
+	# Unable to test:
+	def test_bytes_to_referent_binaryRef(self):
+		self.assertTrue(1 == 1)
+
+	def test_if_node_is_black(self):
+		self.assertTrue(self.root.is_black() == 1)	
+
+	def test_lock_rbttree(self):
+		# Lock storage
+		self.storage.lock()
+		
+		# Get root:
+		root = self.rbtree._follow(self.rbtree._tree_ref)
+		self.assertTrue(1==1)
+
+
+
+
+
+
+
+
 
 """
 		key = 1
---
+++ test_SMTimeSeries.py
@@ -0,0 +1,127 @@
+import unittest
+from TimeSeries import TimeSeries
+from ArrayTimeSeries import ArrayTimeSeries
+from SMTimeSeries import SMTimeSeries
+import sys
+import os
+sys.path.append('./DB/')
+from WrappedDB import WrappedDB
+
+# py.test --doctest-modules  --cov --cov-report term-missing SMTimeSeries.py test_SMTimeSeries.py
+
+# Test cases for the SMTimeSeries class
+class SMTimeSeriesTest(unittest.TestCase):
+
+	def setUp(self):
+		self.values=[1, 3, 0, 1.5, 1]
+		self.times=[1, 2, 4, 5, 10]
+		self.key = "313"
+		self.dbname = "SM_DB.dbdb"
+		self.DB = WrappedDB(self.dbname)
+		self.ts1 = TimeSeries(self.values, self.times)
+		self.ts2 = TimeSeries([1, 2, 3],[0, 5, 10])
+		self.smt1 = SMTimeSeries(self.values, self.times, self.key)
+		self.smt2 = SMTimeSeries([0, 5, 10], [1, 2, 3], "7")
+		
+	def tearDown(self):
+		del self.values
+		del self.times
+		del self.key
+		del self.DB
+		del self.ts1
+		del self.smt1
+		os.remove(self.dbname)
+
+	# Note that these tests are run in sequential order
+
+	def test_init_SMTimeSeries(self):
+		self.assertTrue(type(self.smt1) is SMTimeSeries)
+
+	def test_from_db_with_key(self):
+		self.DB.storeKeyAndTimeSeries(key = self.key, timeSeries = self.ts1)
+		timeSeriesFromDB = self.smt1.from_db(self.key, self.dbname)
+		#timeSeriesFromDB = self.DB.getTimeSeries(self.key)
+		# Check if the fetched SMTimeSeries is the same class as was instantiated:
+		self.assertTrue(timeSeriesFromDB == self.ts1)		
+
+	def test_from_db_without_key(self):
+		self.DB.storeKeyAndTimeSeries(key = self.key, timeSeries = self.ts1)
+		timeSeriesFromDB = self.smt1.from_db(None, self.dbname)
+		#timeSeriesFromDB = self.DB.getTimeSeries(self.key)
+		# Check if the fetched SMTimeSeries is the same class as was instantiated:
+		self.assertRaises(Exception,"Key is not in Database\n")
+
+
+	# Tests for delegation methods:
+	def test_getitem(self):
+		self.assertEqual(self.smt1[2], 3)
+
+	def test_iter(self):
+		index = 0
+		for v in self.smt1:
+			self.assertEqual(v, self.ts1[self.times[index]])
+			index += 1
+
+	def test_len(self):
+		self.assertEqual(len(self.smt1), 5)
+
+	def test_setitem(self):
+		_series = TimeSeries(values=[1, 3, 0, -1.5, -1], times=[1.5, 2, 2.5, 10.5, 3])
+		self.assertEqual(_series[3], -1)
+		_series[3] = 999
+		self.assertEqual(_series[3], 999)
+
+	def test_setitem(self):
+		# Change value at time 1 to 3:
+		self.smt1[1] = 3
+		self.assertEqual(self.smt1[1], 3)
+
+	def test_items(self):
+		self.assertEqual(self.ts1.items(), self.smt1.items())
+
+	def test_iteritems(self):
+		# Get iteritems from timeseries:
+		ts_list = []
+
+		for ts in self.ts1.iteritems():
+			ts_list.append(ts)
+
+		index = 0
+		for v in self.smt1.iteritems():
+			self.assertEqual(v, ts_list[index])
+			index += 1
+
+	def test_itertimes(self):
+		ts_list = []
+
+		for a in self.ts1.itertimes():
+			ts_list.append(a)
+
+		index = 0
+		for v in self.smt1.itertimes():
+			self.assertEqual(v, ts_list[index])
+			index += 1
+
+
+	def test_itervalues(self):
+		ts_list = []
+
+		for a in self.ts1.itervalues():
+			ts_list.append(a)
+
+		index = 0
+		for v in self.smt1.itervalues():
+			self.assertEqual(v, ts_list[index])
+			index += 1
+
+	def test_mean(self):
+		self.assertEqual(self.smt2.mean(), 5.0)
+
+	def test_std(self):
+		self.assertEqual(self.smt2.std(), 4.0824829046386304)
+
+	def test_times(self):
+		self.assertEqual(self.smt1.times(), [1, 2, 4, 5, 10])
+
+	def test_values(self):
+		self.assertEqual(self.smt1.values(), [1, 3, 0, 1.5, 1])
