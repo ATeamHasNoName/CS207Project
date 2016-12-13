1bb4e6de77fbcd5f5c657af5226d8e8002f69146
Leonard Loo <leonard.loo.ks@gmail.com>
2016-12-12 19:11:19 -0500

    Fix directory

--- cs207-2016/labs/treeplot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-dot $1.dot | gvpr -c -ftree.gv | neato -n -Tpng -o $1.png
--- cs207project-1-master/SimSearch/simsearch.sh
+++
@@ -1,5 +0,0 @@
-echo "This script checks the distance of a new time series against a database. Please enter the filename -"
-read filename
-echo "Now, enter the number of top results to return!"
-read topcount
-python Distance_from_known_ts.py $filename $topcount
